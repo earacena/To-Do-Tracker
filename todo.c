feabec1ab0b907d002123edc44a2d11ff28e7012
Emanuel Aracena <earacenadev@gmail.com>
2018-07-14 22:12:38 -0400

    Implemented the remaining functions: clear_list(), save_list(), and load_list(). All the planned features are now implemented and development will move on to testing, refactoring, and polishing.

--- todo.c
+++ todo.c
@@ -3,7 +3,7 @@
 */
 
 /*
-	File description
+	todo.c 
 */
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,6 +12,8 @@
 
 #define MAX_TASKS 50
 #define MAX_TASK_LENGTH 80
+#define MAX_FILENAME_LENGTH 20
+#define BUFFER_SIZE 60
 
 typedef struct List List;
 
@@ -29,6 +31,9 @@ void print_menu()
 	printf("2. Display list\n");
 	printf("3. Remove task\n");
 	printf("4. Mark task complete\n");
+	printf("5. Save list\n");
+	printf("6. Load list\n");
+	printf("7. Clear list\n");
 	printf("0. Quit\n");
 	printf("\n\nChoice?: ");
 }
@@ -40,6 +45,15 @@ void add_task(List **list_ref)
 
 	printf("\n\nNEW TASK: ");
 	fgets(new_task->task_desc, MAX_TASK_LENGTH, stdin);
+
+	uint8_t counter = 0;
+
+	while(counter < MAX_FILENAME_LENGTH) {
+		if (new_task->task_desc[counter] == '\n')
+			new_task->task_desc[counter] = '\0';
+		++counter;
+	}
+
 	new_task->completed = 0;
 	new_task->next = NULL;
 
@@ -103,20 +117,102 @@ void mark_task_complete(List **list_ref)
 	}
 }
 
-void save_list()
+void save_list(List **list_ref)
 {
+	printf("SAVE FILE (max length ~ 20 chars): ");
+	char filename[MAX_FILENAME_LENGTH];
+	fgets(filename, MAX_FILENAME_LENGTH, stdin);
 
-	printf("\n\nFeature not added yet.\n\n");
+	// fgets adds a '\n' making file i/o difficult as the program
+	// cannot write into proper file
+	uint8_t counter = 0;
+
+	while(counter < MAX_FILENAME_LENGTH) {
+		if (filename[counter] == '\n')
+			filename[counter] = '\0';
+		++counter;
+	}
+
+
+	FILE *fptr_out = fopen(filename, "w+");
+	if (fptr_out == NULL) {
+		printf("ERR: Cant open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	List* current = *list_ref;
+
+	if (current == NULL) {
+		printf("ERR: Cannot save an empty list!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (current != NULL) {
+		fprintf(fptr_out, "%" PRId8 " ", current->completed);
+		fprintf(fptr_out, "%s\n", current->task_desc);
+		// For debug
+		//printf("SAVED: '%s'\n", current->task_desc);
+		current = current->next;
+	}
+
+	printf("\n\nSave file '%s' successfully created!\n\n", filename);
+
+	fclose(fptr_out);
 }
 
-void load_list()
+void load_list(List **list_ref)
 {
-	printf("\n\nFeature not added yet.\n\n");
+	printf("LOAD FILE (case sensative): ");
+	char filename[MAX_FILENAME_LENGTH];
+	fgets(filename, MAX_FILENAME_LENGTH, stdin);
+
+	uint8_t counter = 0;
+
+	while(counter < MAX_FILENAME_LENGTH) {
+		if (filename[counter] == '\n')
+			filename[counter] = '\0';
+		++counter;
+	}
+
+	FILE *fptr_in = fopen(filename, "r+");
+
+	uint8_t file_value;
+	char file_task[MAX_TASK_LENGTH];
+
+	List *current = *list_ref;
+
+	while (fscanf(fptr_in, "%" PRId8 " %s", &file_value, file_task) != EOF) {
+		
+		List *new_task = malloc(sizeof(List));
+		strcpy(new_task->task_desc, file_task);
+		new_task->completed = file_value;
+		if (current == NULL ) {
+			*list_ref = new_task;
+			current = new_task;
+		}
+		else if (current->next == NULL) {
+			current->next = new_task;
+			current = current->next;
+		}
+
+		
+	}
+	
+	fclose(fptr_in);
 }
 
-void clear_list()
-{	
-	printf("\n\nFeature not added yet.\n\n");
+void clear_list(List **list_ref)
+{
+	List *current = *list_ref;
+	List *temp_ref;
+
+	while (current != NULL) {
+		temp_ref = current->next;
+		free(current);
+		current = temp_ref;
+	}
+
+	*list_ref = NULL;
 }
 
 void display_list(List **list_ref)
@@ -165,6 +261,16 @@ int main()
 			break;
 		case 4:
 			mark_task_complete(&list);
+			break;
+		case 5:
+			save_list(&list);
+			break;
+		case 6:
+			clear_list(&list);
+			load_list(&list);
+			break;
+		case 7:
+			clear_list(&list);
 			break;
 		default:
 			printf("ERROR: Invalid input, please try again.\n");
